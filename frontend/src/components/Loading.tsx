8c1de93f26f2983e16b388fe37d64f6f93855963
Jawad762 <mjawadzaiter@gmail.com>
2024-02-15 15:32:32 +0200

    trying to make video show up on safari

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,7 +1,7 @@
 const LoadingPage = () => {
   return (
     <section className="grid w-full h-full max-h-screen overflow-hidden place-items-center _bg-pattern-2">
-      <video width="500" height="500" autoPlay muted>
+      <video width="500" height="500" autoPlay muted playsInline>
         <source src="/animated-logo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
